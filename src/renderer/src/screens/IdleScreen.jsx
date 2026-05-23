0b7e0fd275599a5e44cbb7917f053aed3b193823
iveib004 <kkrjenny004@gmail.com>
2026-05-23 19:49:59 +0900

    [feat] idle 화면 거북이 추가

--- src/renderer/src/screens/IdleScreen.jsx
+++ src/renderer/src/screens/IdleScreen.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { Dumbbell, Grid2X2, Pause, Play } from 'lucide-react'
 
-import turtleImage from '../assets/geobugi-turtle.png'
+import { useRive, useStateMachineInput } from '@rive-app/react-canvas'
 
 function IdleScreen({
   realtime,
@@ -16,6 +16,20 @@ function IdleScreen({
   const cumulativeScore = realtime?.cumulative_score ?? 0
   const widgetOpacity = Number(widgetSettings?.opacity ?? 1)
   const widgetScale = Number(widgetSettings?.scale ?? 1)
+  const { rive, RiveComponent } = useRive({
+    src: '/src/assets/turtle.riv',
+    stateMachines: 'State Machine 1',
+    autoplay: true,
+  })
+
+  const neckInput = useStateMachineInput(
+    rive,
+    'State Machine 1',
+    'neck_step'
+  )
+  if (neckInput) {
+    neckInput.value = neckStage
+  } 
 
   return (
     <main className="idle-widget" style={{ opacity: widgetOpacity }}>
@@ -46,8 +60,8 @@ function IdleScreen({
           </button>
         ) : null}
 
-        <div className="idle-turtle" style={{ '--neck-stage': neckStage }}>
-          <img src={turtleImage} alt="" />
+        <div className="idle-turtle">
+          <RiveComponent />
         </div>
       </div>
 
